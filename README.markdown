f74c93967e15b33a143dbc37c47a9999b48bcdb6
Logan V <logan@lstn.net>
2011-12-02 22:50:10 -0600

    initial commit

---
+++ README.markdown
@@ -0,0 +1,43 @@
+# spotify-metadata-php
+This class is used for searches and lookups from the Spotify metadata API.
+
+You can view the Spotify metadata API usage docs at http://developer.spotify.com/en/metadata-api/overview/
+The terms and conditions for this API are available at http://developer.spotify.com/en/metadata-api/terms-of-use/
+
+Some of the code in this class was derived from the metatune library available at http://code.google.com/p/metatune/
+
+## Usage
+
+The API has search and lookup functions for artists, albums, and tracks.
+
+### Lookup
+
+To use the lookup features of this class, call:
+
+	Spotify::lookupArtist(uri)
+	Spotify::lookupAlbum(uri)
+	Spotify::lookupTrack(uri)
+	
+Example script:
+
+	include('spotify.class.php');
+	$spotify = new Spotify;
+	$artist = $spotify->lookupArtist('spotify:artist:4YrKBkKSVeqDamzBPWVnSJ');
+	print_r($artist);
+	
+Another example to list all tracks on an album. The DAO for track and album both support toString()
+
+	include('spotify.class.php');
+	$spotify = new Spotify;
+	$album = $spotify->lookupAlbum('spotify:album:4JKPD5CoWpBbzf2WwokRSp');
+	foreach ($album->tracks as $track) {
+		echo $track."\n";
+	}
+	
+### Search
+
+The search features are accessed via the following functions:
+
+	Spotify::searchArtist(searchquery)
+	Spotify::searchAlbum(searchquery)
+	Spotify::searchTrack(searchquery)
---
+++ spotifymeta.class.php
@@ -0,0 +1,256 @@
+<?php
+class Spotify {
+	const URL_SEARCH = "http://ws.spotify.com/search/1/";
+	const URL_LOOKUP = "http://ws.spotify.com/lookup/1/";
+	
+	private $c; //curl instance
+	private $response; //raw response from spotify
+	
+	public function __construct() {
+		$this->init_curl();
+	}
+	
+	public function init_curl() {
+		$this->c = curl_init();
+		$c =& $this->c;
+		
+		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+	}
+	
+	public function searchTrack($search, $page=1) {
+		$xml = $this->perform_search('track', $search, $page);
+		$tracks = array();
+		foreach ($xml as $e) {
+			//loop through and create Track objects for the search results.
+			$tracks[] = $this->populateTrack($e);
+		}
+		
+		return $tracks;
+	}
+	
+	public function searchArtist($search, $page=1) {
+		$xml = $this->perform_search('artist', $search, $page);
+		$artists = array();
+		foreach ($xml as $e) {
+			$artists[] = $this->populateArtist($e);
+		}
+		return $artists;
+	}
+	
+	public function searchAlbum($search, $page=1) {
+		$xml = $this->perform_search('album', $search, $page);
+		$albums = array();
+		foreach ($xml as $e) {
+			$albums[] = $this->populateAlbum($e);
+		}
+		return $albums;
+	}
+	
+	private function perform_search($type, $search, $page) {
+		$c =& $this->c;
+		$url = $this->getSearchURL($type, $search, $page);
+		curl_setopt($c, CURLOPT_URL, $url);
+		$output = curl_exec($c);
+		$xml = new SimpleXMLElement($output);
+		return $xml;
+	}
+	
+	public function lookupTrack($uri) {
+		$xml = $this->perform_lookup($uri);
+		return $this->populateTrack($xml);
+	}
+	
+	public function lookupArtist($uri, $detail=true) {
+		if ($detail == true) $extras = 'albumdetail';
+		else $extras = null;
+		$xml = $this->perform_lookup($uri, $extras);
+		return $this->populateArtist($xml);
+	}
+	
+	public function lookupAlbum($uri, $detail=true) {
+		if ($detail == true) $extras = 'trackdetail';
+		else $extras = null;
+		$xml = $this->perform_lookup($uri, $extras);
+		return $this->populateAlbum($xml);
+	}
+	
+	private function perform_lookup($uri, $extras=null) {
+		$c =& $this->c;
+		$url = $this->getLookupURL($uri, $extras);
+		curl_setopt($c, CURLOPT_URL, $url);
+		$output = curl_exec($c);
+		$xml = new SimpleXMLElement($output);
+		return $xml;
+	}
+		
+	public function getSearchURL($type, $search, $page=1) {
+		return self::URL_SEARCH.$type.'?q='.$this->translateString($search).$this->addPageSuffix($page);
+	}
+	
+	public function getLookupURL($uri, $extras=null) {
+		if (empty($extras)) $get = '?uri='.$uri;
+		else $get = '?uri='.$uri.'&extras='.$extras;
+		
+		return self::URL_LOOKUP.$get;
+	}
+	
+	private function translateString($string) {
+		// Replace "-" in regular search but leave it on "tag"-searches
+		// such as "genre:brit-pop" or "label:deutsche-grammophon"
+		$string = preg_replace("/(^[^a-z\:]+\-|[\_\(\)])/ui", " ", (trim($string)));
+		
+		// replace multiple whitespaces with a single one.
+		$string = preg_replace("/\s{2,}/", " ", ($string));
+		return urlencode((trim($string)));
+	}
+	
+	private function addPageSuffix($page) {
+		if ($page <= 1 || !is_numeric($page)) return "";
+		return "&page=" . (int) $page;
+	}
+	
+	private function populateTrack($xml) {
+		//populate the track basics
+		$track = new Track;
+		$track->href = (string)$xml->attributes()->href;
+		$track->name = (string)$xml->name;
+		$track->track_number = (int)$xml->{'track-number'};
+		$track->length = (double)$xml->length;
+		$track->popularity = (double)$xml->popularity;
+		$track->available_countries = $this->available_countries((string)$xml->availability->territories);
+		
+		//populate the artist
+		$track->artist = new Artist;
+		$artist =& $track->artist;
+		$artist->name = (string)$xml->artist->name;
+		$artist->href = (string)$xml->artist->attributes()->href;
+		
+		//populate the album info
+		if (!empty($xml->album)) {
+			$track->album = new Album;
+			$album =& $track->album;
+			$album->name = (string)$xml->album->name;
+			$album->href = (string)$xml->album->attributes()->href;
+			$album->released = (string)$xml->album->released;
+			$album->available_countries = $this->available_countries((string)$xml->album->availability->territories);
+		}
+		
+		return $track;
+	}
+	
+	private function populateArtist($xml) {
+		//populate the artist
+		$artist = new Artist;
+		$artist->name = (string)$xml->name;
+		$artist->href = (string)$xml->attributes()->href;
+		$artist->popularity = (double)$xml->popularity;
+		
+		//populate albums if we have some
+		if (sizeOf($xml->albums->album) > 0) {
+			foreach ($xml->albums->album as $a) {
+				$artist->albums[] = $this->populateAlbum($a);
+			}
+		}
+		
+		return $artist;
+	}
+	
+	private function populateAlbum($xml) {
+		//populate the album
+		$album = new Album;
+		$album->name = (string)$xml->name;
+		$album->href = (string)$xml->attributes()->href;
+		$album->released = (string)$xml->released;
+		$album->popularity = (double)$xml->popularity;
+		$album->available_countries = $this->available_countries((string)$xml->availability->territories);
+		
+		//populate artist data
+		$album->artist = new Artist;
+		$artist =& $album->artist;
+		$artist->name = (string)$xml->artist->name;
+		$artist->href = (string)$xml->artist->attributes()->href;
+		
+		//if we have some tracks populate them.
+		if (sizeOf($xml->tracks->track) > 0) {
+			foreach ($xml->tracks->track as $t) {
+				$track = $this->populateTrack($t);
+				$track->album = clone $album;
+				$album->tracks[] = $track;
+			}
+		}
+		
+		return $album;
+	}
+	
+	public function available_countries($string) {
+		$string = trim($string);
+		$countries = explode(' ', $string);
+		$return = array();
+		foreach($countries as $c) {
+			$return[$c] = true;
+		}
+		if (empty($string)) $return = null;
+		return $return;
+	}
+}
+
+class Track extends Spotify {
+	public $href = NULL;
+	public $name = NULL;
+	public $track_number = NULL;
+	public $length = NULL;
+	public $popularity = NULL;
+	
+	public $album = NULL;
+	public $artist = NULL;
+	
+	public $available_countries  = array();
+	
+	public function __construct() { } //no initialization is needed
+	
+	public function __toString() {
+		$string = '';
+		
+		$string = $this->artist->name;
+		
+		$string .= ' - '.$this->album->name;
+		if (!empty($this->album->released)) $string .= " ({$this->album->released})";
+		
+		if (!empty($this->track_number)) $string .= " - ({$this->track_number}) {$this->name}";
+		else $string .= ' - '.$this->name;
+
+		return $string;
+	}
+}
+
+class Album extends Spotify {
+	public $name = NULL;
+	public $href = NULL;
+	public $released = NULL;
+	public $popularity = NULL;
+	
+	public $artist = NULL;
+	public $tracks = array();
+	
+	public $available_countries  = array();
+	
+	public function __construct() { } //no initialization is needed
+	
+	public function __toString() {
+		$string = $this->artist->name.' - '.$this->name;
+		if (!empty($this->released)) $string .= " ({$this->released})";
+		return $string;
+	}
+}
+
+class Artist extends Spotify {
+	public $name = NULL;
+	public $href = NULL;
+	public $popularity = NULL;
+	
+	public $albums = array();
+	
+	public function __construct() { } //no initialization is needed
+}
+?>
